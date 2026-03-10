5c7b83b0eddf4bd5e9e917d1d49c569b8deace77
Hudisson R Xavier <hudissonxavier2018@gmail.com>
2026-03-09 21:36:44 -0300

    Exercíco 02 Concluído: Calcular a quantidade de divisores de um número e listar quais são seu divisores

--- fundamentos/exe02.php
+++ fundamentos/exe02.php
@@ -5,6 +5,11 @@
  * 
  * Execício 02:
  *    Calcular a quantidade de divisores de um número e listar quais são seu divisores.
+ * 
+ * OBS.: A quantidade de divisores de um número negativo é o dobro da quantidade de divisores positivos do seu valor absoluto, pois cada divisor positivo possui também o seu correspondente negativo.
+ * Exemplo:
+ * Os divisores de 6 são: (1, 2, 3, 6).
+ * Os divisores de −6 são: (−1, 1, −2, 2, −3, 3, −6, 6).
  */
 
 
@@ -15,9 +20,24 @@ function numberOfDivisors($numero)
 {
     $quantidadeDeDivisores = 0;
 
-    for($i = 1; $i <= $numero; $i++){
-        if($numero %$i == 0){
-            $quantidadeDeDivisores+=1;
+    # Calcula e retorna o dobro da quantidade de divisores se o número for negativo
+    if ($numero < 0) {
+
+        $numero = $numero * (-1);
+
+        for ($i = 1; $i <= $numero; $i++) {
+            if ($numero % $i == 0) {
+                $quantidadeDeDivisores += 1;
+            }
+        }
+
+        return $quantidadeDeDivisores *=2;
+    }
+
+    # Calcula e retorna a quantidade de divisore se o número for positivo
+    for ($i = 1; $i <= $numero; $i++) {
+        if ($numero % $i == 0) {
+            $quantidadeDeDivisores += 1;
         }
     }
 
@@ -29,8 +49,24 @@ function numberOfDivisors($numero)
  */
 function getDivisors($numero)
 {
-    for($i = 1; $i<= $numero; $i++){
-        if($numero %$i == 0){
+   # Determina os divisores inteiros de um número se o número for negativo
+    if ($numero < 0) {
+
+        $numero = $numero * (-1);
+
+        for ($i = 1; $i <= $numero; $i++) {
+            if ($numero % $i == 0) {
+                $divisores[] = $i;
+                $divisores[] = -$i;
+            }
+        }
+
+        return implode("; ", $divisores);
+    }
+
+    # Determina os divisores inteiros de um número se o número for positivo
+    for ($i = 1; $i <= $numero; $i++) {
+        if ($numero % $i == 0) {
             $divisores[] = $i;
         }
     }
@@ -38,7 +74,7 @@ function getDivisors($numero)
     return implode("; ", $divisores);
 }
 
-$valor = 4;
+$valor = -6;
 
-echo "O número $valor tem ".numberOfDivisors($valor)." divisores <br>";
-echo "Os divisores de $valor são: ".getDivisors($valor);
+echo "O número $valor tem " . numberOfDivisors($valor) . " divisores <br>";
+echo "Os divisores de $valor são: " . getDivisors($valor);
